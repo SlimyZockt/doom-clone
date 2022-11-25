9938db9741539fe3fdd209bbdb346ae637d62c83
SlimyZockt <35144741+SlimyZockt@users.noreply.github.com>
2022-11-25 08:42:36 +0100

    change stuff

--- src/utils/collision.rs
+++ src/utils/collision.rs
@@ -2,11 +2,13 @@ use std::f32::consts::PI;
 
 use color_eyre::{Report, Result};
 
-
-fn raycast(start_pos: [i32; 2], angle: f32) -> Result<bool, Report> {
+fn raycast(start_pos: [f32; 2], angle: f32, map: &[&[i32]]) -> Result<bool, Report> {
     let aTan = angle.atan();
-    if angle > PI {
-        
-    }
+
+    let tile_pos_x = (start_pos[0] / 32.0).ceil();
+    let tile_pos_y = (start_pos[1] / 32.0).ceil();
+    
+    // if (angle == 0 || ra == PI)
+
     Ok(true)
 }
